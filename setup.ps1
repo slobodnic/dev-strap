bacb697630fefe38b2f601316a625ca1e15c3002
Ciprian Slobodnic <ciprian.slobodnic@hrblock.com>
2023-03-09 11:26:20 -0700

    Removed the winget script

--- setup.ps1
+++ setup.ps1
@@ -25,7 +25,6 @@ Expand-Archive scripts.zip -DestinationPath .
 Write-Host -ForegroundColor Green "Installing pre-requisites" 
 # The pre-requisit scripts are in order in which they need to install
 
-& $scriptsPath\dev-strap-main\pre-requisites\winget.ps1
 & $scriptsPath\dev-strap-main\pre-requisites\dotnet-sdk.ps1
 & $scriptsPath\dev-strap-main\pre-requisites\chocolatey.ps1
 & $scriptsPath\dev-strap-main\pre-requisites\chocolatey.ps1
