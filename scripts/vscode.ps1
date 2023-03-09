7fbd82c2cd047ee51e262c61c3ae6b74abedc12d
Ciprian Slobodnic <ciprian.slobodnic@hrblock.com>
2023-03-09 12:31:47 -0700

    Updated the VSCode script

--- scripts/vscode.ps1
+++ scripts/vscode.ps1
@@ -1,1 +1,16 @@
-Install-Script -Name Install-VSCode
+$channel = 'stable'
+$platform = 'win32-x64-user' 
+$SourceURL = "https://code.visualstudio.com/sha/download?build=$channel&os=$platform";
+$Installer = $env:TEMP + "\vscode.exe"; 
+
+if (Get-Command "code1" -errorAction SilentlyContinue)
+{    
+    Write-Host -ForegroundColor Yellow "VSCode already exists... skipping" 
+} else {
+    Write-Host -ForegroundColor Green "Installing VSCode"
+
+    Invoke-WebRequest $SourceURL -OutFile $Installer;
+    Start-Process -FilePath $Installer -Args "/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath" -Wait; 
+    Remove-Item $Installer;
+    Stop-Process -Name Explorer   
+}
