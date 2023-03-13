e30145d2f6068b474d5a94c49a3cac96c00f20dd
Ciprian Slobodnic <ciprian.slobodnic@hrblock.com>
2023-03-13 13:37:43 -0600

    Still working on the dotnet command not being available

--- pre-requisites/python.ps1
+++ pre-requisites/python.ps1
@@ -6,7 +6,7 @@ if (Get-Command "pip" -errorAction SilentlyContinue)
 
     Invoke-WebRequest -uri https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe -OutFile python-setup.exe
     .\python-setup.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-    del .\python-setup.exe
+    #del .\python-setup.exe
 }
 
 Write-Host -ForegroundColor Green "Upgrading pip" 
