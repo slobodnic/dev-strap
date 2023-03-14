086181728553eddda437480ae8acdae7adf74db5
Ciprian Slobodnic <ciprian.slobodnic@hrblock.com>
2023-03-14 14:13:24 -0600

    Renamed the plugins file to vs-plugins for clarity

--- scripts/vscode.ps1
+++ scripts/vscode.ps1
@@ -15,10 +15,10 @@ if (Get-Command "code" -errorAction SilentlyContinue)
 
 Write-Host -ForegroundColor Green "Installing VSCode addons"
 
-if (Test-Path -Path .\plugins.txt -PathType Leaf) {
+if (Test-Path -Path .\vs-plugins.txt -PathType Leaf) {
     $existingPlugins = code --list-extensions;
 
-    foreach($plugin in Get-Content .\plugins.txt) {
+    foreach($plugin in Get-Content .\vs-plugins.txt) {
         if ($existingPlugins -contains $plugin) {
             Write-Host -ForegroundColor Yellow $plugin" already installed. Skipping ..."
         } else {
@@ -27,5 +27,5 @@ if (Test-Path -Path .\plugins.txt -PathType Leaf) {
         }
     }
 } else {
-    Write-Host -ForegroundColor Yellow "plugins.txt file does not exists"
+    Write-Host -ForegroundColor Yellow "vs-plugins.txt file does not exists"
 }
