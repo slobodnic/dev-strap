3092f382365d658b733d7c6113055f3f9692f1b1
Ciprian Slobodnic <ciprian.slobodnic@hrblock.com>
2023-03-14 13:10:54 -0600

    Added validation if total commander is installed

--- optional/totalcommander.ps1
+++ optional/totalcommander.ps1
@@ -1,4 +1,9 @@
 Write-Host -ForegroundColor Green "Installing Total Commander"
 
-choco install -y totalcommander
-refreshenv
+if (choco list totalcommander -l | Select-String -pattern "1 packages installed." -quiet){
+    Write-Host -ForegroundColor Yellow "Total Commander already install... skipping" 
+} else {
+    Write-Host -ForegroundColor Green "Installing Total Commander"
+    choco install -y totalcommander
+    refreshenv
+}
