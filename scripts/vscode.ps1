3384947145fbe78e5e954060cac97b413713f3e2
Ciprian Slobodnic <slobodnic@gmail.com>
2023-03-13 20:55:59 -0600

    Added plugins installation

--- scripts/vscode.ps1
+++ scripts/vscode.ps1
@@ -12,3 +12,35 @@ if (Get-Command "code" -errorAction SilentlyContinue)
     choco install -y vscode
     refreshenv
 }
+
+$plugins = "bierner.markdown-mermaid", `
+            "ccimage.jsonviewer", `
+            "dagra.jsonschemautils", `
+            "hediet.vscode-drawio", `
+            "marcoq.vscode-typescript-to-json-schema", `
+            "Meezilla.json", `
+            "mindaro-dev.file-downloader", `
+            "mindaro.mindaro", `
+            "mohsen1.prettify-json", `
+            "ms-azuretools.vscode-docker", `
+            "ms-dotnettools.csharp", `
+            "ms-kubernetes-tools.vscode-kubernetes-tools", `
+            "ms-python.python", `
+            "ms-python.vscode-pylance", `
+            "ms-toolsai.jupyter", `
+            "ms-toolsai.jupyter-keymap", `
+            "ms-toolsai.jupyter-renderers", `
+            "ms-toolsai.vscode-jupyter-cell-tags", `
+            "ms-toolsai.vscode-jupyter-slideshow", `
+            "ms-vscode-remote.remote-containers", `
+            "nickdemayo.vscode-json-editor", `
+            "oliversturm.fix-json", `
+            "redhat.vscode-yaml", `
+            "streetsidesoftware.code-spell-checker", `
+            "tomoyukim.vscode-mermaid-editor"
+
+
+foreach ($plugin in $plugins) {
+    code --install-extension $plugin
+}
+
