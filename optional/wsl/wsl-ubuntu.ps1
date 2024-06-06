3991db854bda5bc8688ba44f588089241744787a
Ciprian Slobodnic <slobodnic@gmail.com>
2024-06-06 12:04:55 -0600

    Changes to make the Ubuntu distro default and run strap

--- optional/wsl/wsl-ubuntu.ps1
+++ optional/wsl/wsl-ubuntu.ps1
@@ -3,4 +3,6 @@ $distros = wsl.exe --list --quiet
 if ($distros -eq $null -Or -Not $distros.Contains("Ubuntu")) {
     # Ubuntu distro not found
     wsl --install -d Ubuntu;
+    wsl -s Ubuntu;
+    wsl -e ./optional/wsl/strap.sh
 }
